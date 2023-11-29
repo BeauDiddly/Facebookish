18cb6a85484c3e434f7655c20182dae721308be9
Heston <heston.cv@gmail.com>
2023-11-29 17:22:38 -0600

    Impliment user profile image and ability to change it

--- server/app/my_page.py
+++ server/app/my_page.py
@@ -2,6 +2,8 @@ from app import db
 from flask import (
     render_template, request, session, redirect, Blueprint, url_for, flash, jsonify
 )
+from werkzeug.utils import secure_filename
+import os
 
 from .models import User, Post
 
@@ -30,7 +32,8 @@ def user_page():
     feed.reverse()
     db.session.commit()
 
-    return render_template("home.html", feed=feed, bio=current_user.bio)
+
+    return render_template("home.html", feed=feed, bio=current_user.bio, profile_image_url=current_user.image_url)
 
 @bp.route('/<string:username>/update_bio', methods=['POST'])
 def update_bio(username):
@@ -41,3 +44,30 @@ def update_bio(username):
         user.bio = bio
         db.session.commit()
         return redirect(url_for('my_page.user_page'))
+
+@bp.route('/<string:username>/update_image', methods=['POST'])
+def update_image(username):
+    print("Entered update_image function")
+    user = User.query.filter_by(username=username).first()
+
+    if user and 'image' in request.files:
+        file = request.files['image']
+        if file:
+            filename = secure_filename(file.filename)
+            print('filename', filename)
+
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            upload_folder = os.path.join(base_dir, 'static', 'upload', 'images')
+            if not os.path.exists(upload_folder):
+                os.makedirs(upload_folder)
+
+            file_path = os.path.join(upload_folder, filename)
+            print(f"Saving file to: {file_path}")
+            file.save(file_path)
+
+            relative_path = os.path.join('upload', 'images', filename)
+            user.image_url = relative_path
+            db.session.commit()
+
+
+    return redirect(url_for('my_page.user_page'))
