46f73fba7b444137974f69811e7c60f3e20c00c9
Beau Schureck <beauschureck@gmail.com>
2023-11-29 17:01:08 -0600

    go to desktop

--- server/tests/auth_test.py
+++ server/tests/auth_test.py
@@ -1,8 +1,25 @@
-
+from app import db
 
 def test_login(client):
-    response = client.post("/login", data={
-        "username": "Beau",
-        "password": "swagmonster",
+    response = client.post("/auth/login", data={
+        "username": "Beaudemann",
+        "password": "ach",
+    })
+    assert response.status_code == 302
+
+def test_register1(client):
+    response = client.post("/auth/register", data={
+        "username": "hungryboi",
+        "password": "ach",
+        "confirm_password": "ach",
+    })
+    db.session.delete()
+    assert response.status_code == 302
+
+def test_register2(client):
+    response = client.post("/auth/register", data={
+        "username": "Beaudemann",
+        "password": "ach",
+        "confirm_password": "ach",
     })
     assert response.status_code == 200
